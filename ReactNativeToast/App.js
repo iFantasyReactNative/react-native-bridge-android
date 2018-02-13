2762ddcebd87105fd078db492022de16adef9c34
Imagine Chiu <chihfan.chiu@igroup.tw>
2018-02-13 09:49:32 +0800

    使用Package的方法, 將App.js 更改為範例程式碼使用

--- ReactNativeToast/App.js
+++ ReactNativeToast/App.js
@@ -6,52 +6,62 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Button,
+    Platform,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
+import MyAndroidToast from 'react-native-my-android-toast';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
+    ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
+    android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
 export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+
+    showToast = () => {
+        console.log('MyAndroidToast=>', MyAndroidToast);
+        MyAndroidToast.show('My Project is TOSTED!', MyAndroidToast.SHORT);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+    <Text style={styles.welcome}>
+        Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit App.js
+        To get started, edit App.js
         </Text>
         <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+        {instructions}
+    </Text>
+        {/* 建立一個 Button 元件，以便呼叫 Toast */}
+    <Button onPress={this.showToast} title="TOAST ME!"></Button>
+            </View>
     );
-  }
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
